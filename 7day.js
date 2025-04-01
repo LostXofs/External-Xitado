1dab5c96a1bf189cddcfc0869ef6104f58d326ea
LostXofs <lostmmma@gmail.com>
2025-04-01 10:05:11 +0100

    Create 7day.js

---
+++ 7day.js
@@ -0,0 +1,12 @@
+loopy() 
+ function loopy() { 
+ var sWord ="@LostXofs" 
+ while (sWord != "Escopar") {// Pass
+ sWord = prompt("Enter The Uesr ")  
+ } 
+ 
+//confirm("ok");
+window.alert(" Expires : 2025/04/08  ");
+window.alert(" Welcome To Panel  ");
+
+}
