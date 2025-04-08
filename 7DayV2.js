5666802179b5c59895dcab1070a0d6ffc145e0e1
LostXofs <lostmmma@gmail.com>
2025-04-08 14:01:28 +0100

    Create 7DayV2.js

---
+++ 7DayV2.js
@@ -0,0 +1,12 @@
+loopy() 
+ function loopy() { 
+ var sWord ="@LostXofs" 
+ while (sWord != "K-Week-AJXBJS") {// Pass
+ sWord = prompt("Enter The Uesr ")  
+ } 
+ 
+//confirm("ok");
+window.alert(" Expired : 2025/04/15 ");
+window.alert(" Welcome To The Panel ");
+
+}
