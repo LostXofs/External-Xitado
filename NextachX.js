6bd060f772b7c4ec690c849aacd0e490b77b3ed7
LostXofs <lostmmma@gmail.com>
2025-04-09 10:37:23 +0100

    Create NextachX.js

---
+++ NextachX.js
@@ -0,0 +1,13 @@
+loopy() 
+ function loopy() { 
+ var sWord ="@LostXofs" 
+ while (sWord != "NextashX") {// Pass
+ sWord = prompt("Enter The Uesr ")  
+ } 
+ 
+//confirm("ok");
+   
+window.alert("  Expired : Lift Time  ");
+window.alert(" Welcome To Panel ");
+
+}
