37cf0ef0c8a39e0c17770816cc0b97c5dfbb6612
LostXofs <lostmmma@gmail.com>
2025-04-18 16:24:02 +0100

    Create Lilyan.js

---
+++ Lilyan.js
@@ -0,0 +1,12 @@
+loopy() 
+ function loopy() { 
+ var sWord ="@LostXofs" 
+ while (sWord != "KMK") {// Pass
+ sWord = prompt("Enter The Uesr ")  
+ } 
+ 
+//confirm("ok");
+window.alert(" Expired : 2025/04/15 ");
+window.alert(" Welcome To The Panel ");
+
+}
