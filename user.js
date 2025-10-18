601f9c1f84ecc7df24a3eaf414cdaaa1187e98d7
LostXofs <lostmmma@gmail.com>
2025-10-18 18:52:44 +0100

    Create user.js

---
+++ user.js
@@ -0,0 +1,12 @@
+loopy() 
+ function loopy() { 
+ var sWord ="@LostXofs" 
+ while (sWord != "lost") {// Pass
+ sWord = prompt("Enter The Uesr ")  
+ } 
+ 
+//confirm("ok");
+window.alert(" Expired : 2025/10/28 ");
+window.alert(" Welcome To The Panel ");
+
+}
